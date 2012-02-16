d2cf602b96aa5626c3545ea908b05c005fd14761
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-02-16 16:56:51 +0100

    Changed template engine from ejs to jade

--- config/environments/all.js
+++ config/environments/all.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 module.exports = function() {
   this.set('views', __dirname + '/../../app/views');
-  this.set('view engine', 'ejs');
+  this.set('view engine', 'jade');
   this.use(express.logger());
   this.use(express.bodyParser());
   this.use(this.router);
